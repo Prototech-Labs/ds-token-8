98915df7eb12ce88b93012a24997332f8c60e682
Brian McMichael <brian@brianmcmichael.com>
2023-10-19 11:36:32 -0400

    Move auth

--- src/token.sol
+++ src/token.sol
@@ -14,63 +14,9 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 pragma solidity ^0.8.21;
 
-interface DSAuthority {
-    function canCall(
-        address src, address dst, bytes4 sig
-    ) external view returns (bool);
-}
-
-contract DSAuthEvents {
-    event LogSetAuthority (address indexed authority);
-    event LogSetOwner     (address indexed owner);
-}
-
-contract DSAuth is DSAuthEvents {
-    DSAuthority  public  authority;
-    address      public  owner;
-
-    constructor() {
-        owner = msg.sender;
-        emit LogSetOwner(msg.sender);
-    }
-
-    function setOwner(address owner_)
-        public
-        auth
-    {
-        owner = owner_;
-        emit LogSetOwner(owner);
-    }
-
-    function setAuthority(DSAuthority authority_)
-        public
-        auth
-    {
-        authority = authority_;
-        emit LogSetAuthority(address(authority));
-    }
-
-    modifier auth {
-        require(isAuthorized(msg.sender, msg.sig), "ds-auth-unauthorized");
-        _;
-    }
-
-    function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
-        if (src == address(this)) {
-            return true;
-        } else if (src == owner) {
-            return true;
-        } else if (authority == DSAuthority(address(0))) {
-            return false;
-        } else {
-            return authority.canCall(src, address(this), sig);
-        }
-    }
-}
-
+import {DSAuth} from "./auth.sol";
 
 contract DSToken is DSAuth {
     bool                                              public  stopped;
